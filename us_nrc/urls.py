a5d7ac4090f4bcfa66a18b478ded0e6aa6e86f79
pooja97 <poojayadav2124@gmail.com>
2023-09-28 01:23:00 -0700

    minor changes

--- us_nrc/urls.py
+++ us_nrc/urls.py
@@ -4,7 +4,7 @@ from us_nrc.views import reactor_state
 from us_nrc.views import reactor_name,outage,outage_details
 
 urlpatterns = [
-    # path('',read_data.index,name="index"),
+    path('',read_data.index,name="index"),
     path(r'outage_details/',outage_details.ReactorOutageDetails.as_view()),
     path(r'reactor_outage/',outage.ReactorOutageView.as_view()),
     path(r'search/',reactor_name.reactorName.as_view()),
